5774666cafac6c68d556f53fe1b1e81424efbbf8
Stuart Weenig <stuart@weenig.com>
2020-03-23 15:16:48 +0000

    Added grouping by country and reverted graphs to line (vs. stack)

--- COVID19_by_State/ad.groovy
+++ COVID19_by_State/ad.groovy
@@ -1,8 +1,16 @@
 import groovy.json.JsonSlurper
 jsondata = new URL("https://finnhub.io/api/v1/covid19/us?token=${hostProps.get('finnhub.api.key')}").getText()
 objectdata = new JsonSlurper().parseText(jsondata)
+countries = [
+    canada: ["Alberta","British Columbia","Manitoba","New Brunswick","Newfoundland and Labrador","Northwest Territories","Nova Scotia","Nunavut","Ontario","Prince Edward Island","Quebec","Saskatchewan","Yukon"],
+    ships: ["Diamond Princess", "Grand Princess","Wuhan Evacuee"]
+]
+
 objectdata.each{
   state = it.state.trim()
-  if (state.size() > 0){println("${state.replaceAll(/[\s,\.]/,"")}##${state}")}
+  if (countries.canada.contains(state)) {country = "Canada"}
+  else if (countries.ships.contains(state)) {country = "Other"}
+  else country = "US"
+  if (state.size() > 0){println("${state.replaceAll(/[\s,\.]/,"")}##${state}######country=${country}")}
 }
 return 0
