a2a6ccd92376d08b5c0db1c4e164a97cae6b41cf
Stuart Weenig <stuart@weenig.com>
2020-03-30 11:16:56 -0500

    Simplified collection script down to 9 lines.

--- LookerStatus/collect.groovy
+++ LookerStatus/collect.groovy
@@ -1,6 +1,4 @@
-hostname = hostProps.get("system.hostname")
-page = new URL("https://${hostname}/availability").getText()
-// println(page + "\n" + "=".multiply(80))
+page = new URL("https://${hostProps.get("system.hostname")}/availability").getText()
 pattern = /<h2 class="lk-simple-title">Looker is (.*)<\/h2>/
 if ((page =~ pattern).size() > 0){ //this might be doable with elvis, but this works
   if ((page =~ pattern)[0][1].toLowerCase() == "up") {
