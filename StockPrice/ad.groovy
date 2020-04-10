9720f2c550723516e3b4568eefda729beb2df0f3
Stuart Weenig <stuart@weenig.com>
2020-04-10 08:56:56 -0500

    Updated since endpoint is now premium.

--- StockPrice/ad.groovy
+++ StockPrice/ad.groovy
@@ -1,14 +1,15 @@
 import groovy.json.JsonSlurper
 stocks = hostProps.get("stocks").tokenize(",| ")
 token = hostProps.get("finnhub.api.key")
-stocks.each{
-  try {
-    url = "https://finnhub.io/api/v1/stock/profile?token=${token}&symbol=${it}"
-    // println(url)
-    jsondata = url.toURL().getText()
-    data = new JsonSlurper().parseText(jsondata)
-    // println(data)
-    println("${it}##${data.name}##${data.weburl}####address=${data.address} ${data.city} ${data.state}&phone=${data.phone}&exchange=${data.exchange}&currency=${data.currency}")
-  } catch (Exception e) {println(e);return 1}
-}
+try {
+  url = "https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${token}"
+  jsondata = url.toURL().getText()
+  // println(jsondata)
+  data = new JsonSlurper().parseText(jsondata)
+  data.each{
+     if (stocks.contains(it.symbol)) {
+       println("${it.symbol}##${it.description}")
+     }
+  }
+} catch (Exception e) {println(e);return 1}
 return 0
