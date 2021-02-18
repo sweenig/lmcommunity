2e409e5a76b871505f3f5cb06819b558c91a7d9d
Stuart Weenig <stuart@weenig.com>
2021-02-18 09:46:26 -0600

    Publishing first version. Readme upon request.

---
+++ LMAutomation/WebCheckLocations/ad.groovy
@@ -0,0 +1,9 @@
+import groovy.json.JsonSlurper
+
+page = new URL("https://s3.amazonaws.com/logicmonitor-marketing/sitemonitor_ips.json?_ga=2.37320611.289127943.1612793504-851486786.1607022888").getText()
+
+instances = new JsonSlurper().parseText(page)
+
+instances.Logicmonitor_Sitemonitor_Machines.Public_IPs.each{println("${it.ip}_${it.location.replaceAll(" ","")}##${it.location} (${it.ip})")}
+
+return 0
